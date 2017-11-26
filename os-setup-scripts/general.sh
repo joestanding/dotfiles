abe71c2278eadcb58a8f3dd1dfa0664dd85e60cf
joestanding <joe.at.g1@gmail.com>
2017-11-26 12:36:16 +0000

    Change copies to symlinks

--- os-setup-scripts/general.sh
+++ os-setup-scripts/general.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
 echo "[*] Configuring vim"
-cp ~/git/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/git/dotfiles/vim/.vimrc ~/.vimrc
 ~/git/dotfiles/vim/install-vundle.sh
 
 echo "[*] Configuring i3"
 mkdir -p ~/.config/i3/
-cp ~/git/dotfiles/i3/config ~/.config/i3/config
+ln -s ~/git/dotfiles/i3/config ~/.config/i3/config
 
 echo "[*] Configuring Termite"
 mkdir -p ~/.config/termite
-cp ~/git/dotfiles/terminals/termite/config ~/.config/termite/config
+ln -s ~/git/dotfiles/terminals/termite/config ~/.config/termite/config
 
 echo "[*] Configuring Polybar"
-mkdir -p ~/.config/polybar
-cp ~/git/dotfiles/polybar/* ~/.config/polybar
+ln -s ~/git/dotfiles/polybar ~/.config/polybar
 
 echo "[*] Configuring Compton"
-cp ~/git/dotfiles/compton/compton.conf ~/.config/compton.conf
+ln -s ~/git/dotfiles/compton/compton.conf ~/.config/compton.conf
 
 echo "[*] Copying resources"
-cp -R ~/git/dotfiles/resources ~/.resources
+ln -s ~/git/dotfiles/resources ~/.resources
 
 echo "[*] Configuring zsh"
-cp ~/git/dotfiles/zsh/.zshrc ~/
+ln -s ~/git/dotfiles/zsh/.zshrc ~/
 
 echo "[*] Configuring gtk"
 mkdir -p ~/.config/gtk-3.0
 echo "vte-terminal { padding: 10px 10px 10px 10px; }" > ~/.config/gtk-3.0/gtk.css
 
-echo "[*] Installing gef for gdb"
-wget -q -O- https://github.com/hugsy/gef/raw/master/gef.sh | sh
+#echo "[*] Installing gef for gdb"
+#wget -q -O- https://github.com/hugsy/gef/raw/master/gef.sh | sh
 
 echo "[*] Setting i3 as the default window manager"
 echo 'exec i3' > ~/.xinitrc
