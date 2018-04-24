39b9222dfb1c23774740cd5e6b07e79a33306acb
joestanding <me@joestanding.co.uk>
2018-04-24 21:55:37 +0100

    Added SSH exception to iptables

--- os-setup-scripts/iptables.sh
+++ os-setup-scripts/iptables.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Allow SSH, in particular already established sessions
+sudo iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+
+# By default allow all outbound, prohibit all inbound
 sudo iptables -P INPUT DROP
 sudo iptables -P FORWARD DROP
 sudo iptables -P OUTPUT ACCEPT
+
+# Allow unrestricted traffic on the loopback
 sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A OUTPUT -o lo -j ACCEPT
