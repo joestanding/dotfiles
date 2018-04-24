0aec9dfad09f3d3eaf909dae7fd45cb0c804bb88
joestanding <me@joestanding.co.uk>
2018-04-24 22:00:01 +0100

    Allow inbound connections that are the result of outbound connections, and flush any existing rules at the start

--- os-setup-scripts/iptables.sh
+++ os-setup-scripts/iptables.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Purge any existing iptables rules
+sudo iptables -F
+sudo iptables -X
+
+# Allow any inbound connections that are the result of an outbound connection
+sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
 # Allow SSH, in particular already established sessions
 sudo iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
